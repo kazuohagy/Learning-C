49ad87171b5174d8486bb2240962dc3b8c6cc180
Gustavo Kazuo <73189635+kazuohagy@users.noreply.github.com>
2022-11-23 22:07:06 -0300

    initial commit :tada:

---
+++ Aula01/aula01.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+class Principal
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello World!");
+    }
+
+}
